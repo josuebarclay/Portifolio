afb5007e501447cbfd84ba0fb82e4e73645f6082
Barclay Josué <josuebarclay@hotmail.com>
2022-09-29 17:21:06 -0400

    commit

--- teste1-main/script.js
+++ teste1-main/script.js
@@ -1,3 +1,20 @@
  document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+
+function atualizarPreco () {
+    const qtde = document.querySelector("#qtde").value 
+    const temJS = document.querySelector("#js").checked 
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    let preco = qtde *100;
+    if (temJS) preco *= 1.1
+    if (incluiLayout) {
+        preco = preco + 500 
+    }
+    document.querySelector("#preco").innerHTML = "R$" +preco .toFixed(2)
+
+};
